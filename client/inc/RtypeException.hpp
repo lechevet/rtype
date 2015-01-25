bb162ad8f26d44e012af0a7ce1452ee11b7fcfd2
Loïc Echevet <loic.echevet@epitech.eu>
2015-01-25 12:07:24 -0800

    clean

---
+++ client/inc/RtypeException.hpp
@@ -0,0 +1,32 @@
+#ifndef		RTYPEEXCEPTION_HH
+# define	RTYPEEXCEPTION_HH
+
+# include	<sstream>
+# include	<errno.h>
+
+class		RtypeException : public std::exception
+{
+public:
+
+  RtypeException(const char *Msg)
+  { 
+    std::ostringstream oss; 
+    oss << "Error : " << Msg; 
+    this->msg = oss.str(); 
+  } 
+  
+  virtual ~RtypeException() throw() 
+  { 
+  
+  } 
+  
+  virtual const char * what() const throw() 
+  { 
+    return this->msg.c_str(); 
+  } 
+  
+private: 
+  std::string msg;
+};
+
+#endif
